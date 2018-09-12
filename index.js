0fb5d32a18709f2882e4bad74581be4b32a0e6f7
Caroline  Lee <carolinesoleelee@gmail.com>
2018-09-12 18:54:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 function theBeatlesPlay(musicians,instruments){
+  var instruments[ 'John Lennon plays Guitar', 'Paul McCartney plays Bass Guitar', 'George Harrison plays Lead Guitar', 'Ring Starr plays Drums']
 }
